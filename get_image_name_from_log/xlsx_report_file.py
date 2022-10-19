38ded0aea9f6c7fa1ec10cd9eaa63018c8991d97
evgeniy <pavlinl4@gmail.com>
2022-10-19 10:42:27 +0300

    change "image_name" to "image_file_name"

--- get_image_name_from_log/xlsx_report_file.py
+++ get_image_name_from_log/xlsx_report_file.py
@@ -6,24 +6,23 @@ from pathlib import Path
 
 
 def create_report_file():
-    report_date = datetime.today().strftime("%Y-%m-%d")
+    # report_date = datetime.today().strftime("%Y-%m-%d")
     path_to_file = f'{report_folder}/{report_file_name}'
 
     if os.path.exists(path_to_file):
         wb = load_workbook(path_to_file)  # файл есть и открываю его
         ws = wb['images']
-        # ws = wb.create_sheet(report_date)  # добавляю новую таблицу
     else:
         wb = Workbook()  # если файда еще нет
         ws = wb.active  # если файа еще нет
         ws.title = 'images'  # если файда еще нет
 
-    ws.column_dimensions['A'].width = 20
+    ws.column_dimensions['A'].width = 30
     ws.column_dimensions['B'].width = 20
     ws.column_dimensions['C'].width = 20  # задаю ширину колонки
     ws.column_dimensions['D'].width = 20
 
-    ws['A1'] = 'image_name'  # create columns names
+    ws['A1'] = 'image_file_name'  # create columns names
     ws['B1'] = 'Ъ'
     ws['C1'] = 'PXP'
     ws['D1'] = 'TASS'
@@ -32,19 +31,19 @@ def create_report_file():
     return path_to_file
 
 
-def write_to_file(path_to_file, image_name, column_name, line_number):
+def write_to_file(path_to_file, image_file_name, column_name, line_number):
     wb = load_workbook(path_to_file)
     ws = wb["images"]
-    ws[f'A{line_number}'] = image_name
-    ws[f'{column_name}{line_number}'] = '&&&'
+    ws[f'A{line_number}'] = image_file_name
+    ws[f'{column_name}{line_number}'] = '*****'
     wb.save(path_to_file)
 
 
-def find_row_with_image_name(image_name, path_to_report_file):  # return row number containing image_name
+def find_row_with_image_name(image_file_name, path_to_report_file):  # return row number containing image_file_name
     wb = load_workbook(path_to_report_file)
     ws = wb["images"]
     for cell in ws['A']:
-        if cell.value == image_name:
+        if cell.value == image_file_name:
             return cell.row
     return None
 
@@ -60,14 +59,14 @@ if __name__ == '__main__':
     report_folder = Path.home() / "Documents" / "photo_upload_logs"
     report_file_name = 'uploaded_images.xlsx'
 
-    image_name = '20171024_PAV5943_test_22.JPG'
+    image_file_name = '20171024_PAV5943_test_22.JPG'
     column_name = 'B'
 
     path_to_report_file = create_report_file()
 
-    row_number = find_row_with_image_name(image_name, path_to_report_file)
+    row_number = find_row_with_image_name(image_file_name, path_to_report_file)
 
-    if row_number is None: # if no image_name in "A" column write data to new column
+    if row_number is None: # if no image_file_name in "A" column write data to new column
         row_number = find_last_row(path_to_report_file) + 1
 
-    write_to_file(path_to_report_file, image_name, column_name, row_number)
+    write_to_file(path_to_report_file, image_file_name, column_name, row_number)
