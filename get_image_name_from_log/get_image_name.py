2502f8453f86ab8b253e7b941ad3f01d28329c74
evgeniy <pavlinl4@gmail.com>
2022-10-19 11:01:24 +0300

    add function to column name

--- get_image_name_from_log/get_image_name.py
+++ get_image_name_from_log/get_image_name.py
@@ -1,5 +1,7 @@
 import re
 
+from xlsx_report_file import create_report_file, find_row_with_image_name, find_last_row, write_to_file
+
 
 def find_name(log_line):
     pattern = r'(?<=/)[A-Za-z0-9_-]+\.(JPG|jpg)'
@@ -8,17 +10,31 @@ def find_name(log_line):
         return re.search(pattern, log_line).group(), re.search(ftp_pattern, log_line).group()
 
 
-def image_name(path_to_file):  # return list of lines
+def image_name(path_to_file):  # return list of lines from log file
     with open(path_to_file, 'r') as log_file:
         log_lines = log_file.readlines()
     return log_lines
 
 
-def main(path_to_file):
-    log_lines = image_name(path_to_file)
+def get_column_name(ftp_name):  # return colum's letter for ftp address
+    ftp_dict = {"ftp.kommersant.ru": "B",  # check ftp address !!!!!
+                "ftp.tass.ru": "D",
+                "ftp.pxp.ru": "C"}
+    return ftp_dict[ftp_name]
+
+
+def main(path_to_log_file):
+    path_to_report_file = create_report_file()  # create report file, scip if exist
+    log_lines = image_name(path_to_log_file)
     for log_line in log_lines:
         if find_name(log_line) is not None:
-            print(find_name(log_line))
+            image_file_name, ftp_name = find_name(log_line)
+            print(find_name(log_line))  # get file name and ftp address
+            row_number = find_row_with_image_name(image_file_name, path_to_report_file)  # get row to write
+            column_name = get_column_name(ftp_name)
+            if row_number is None:  # if no image_file_name in "A" column write data to new column
+                row_number = find_last_row(path_to_report_file) + 1
+            write_to_file(path_to_report_file, image_file_name, column_name, row_number)
 
 
 if __name__ == '__main__':
